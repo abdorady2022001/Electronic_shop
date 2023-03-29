4de0a6eb065633cb587d99a1dd00ba64ecb2e2bd
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-29 09:23:42 +0200

    Registration page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_application_1/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: login(),
+    );
   }
 }
