0e73d91474420e50f8ca0096c565feab1a8b36ab
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 12:09:56 +0200

    email field

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class login extends StatelessWidget {
@@ -14,6 +16,24 @@ class login extends StatelessWidget {
               const SizedBox(
                 height: 64,
               ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
+                  )),
               TextField(
                   keyboardType: TextInputType.text,
                   obscureText: true,
