f798fe58aafb9a2bf798e904ac143e77705fd473
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 21:57:34 +0200

    I shortened the steps and copied the codes from the login

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,16 +1,75 @@
-import 'package:flower_app/pages/login.dart';
-import 'package:flower_app/shared/colors.dart';
-import 'package:flower_app/shared/contants.dart';
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class Register extends StatelessWidget {
-  const Register({super.key});
+import 'package:flutter/material.dart';
+import 'package:shop_application_1/shaerd/colors.dart';
+import 'package:shop_application_1/shaerd/contants.dart';
+
+class login extends StatelessWidget {
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: decorationTextfield.copyWith(
+                        hintText: "Enter Your Email : ")),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password :",
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                  child: Text(
+                    "sign in",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Do not have an account?",
+                        style: TextStyle(fontSize: 18)),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text('sign up',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18))),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
