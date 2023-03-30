c51397e98ae66dd51e945fdb917ccadb30d2138e
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 22:00:38 +0200

    I worked import Register in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_application_1/pages/login.dart';
+import 'package:shop_application_1/pages/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: login(),
+      home: Register(),
     );
   }
 }
