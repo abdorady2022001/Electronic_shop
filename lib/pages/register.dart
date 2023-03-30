f847ca3f79ce007b3da84040b730cea837e2e83c
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 21:51:00 +0200

    I worked class Register

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -2,3 +2,12 @@ import 'package:flower_app/pages/login.dart';
 import 'package:flower_app/shared/colors.dart';
 import 'package:flower_app/shared/contants.dart';
 import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
