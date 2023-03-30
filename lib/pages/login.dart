f93cd71fc7e97a25b44235cf2229238b03900e16
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 14:31:56 +0200

    Make a text link

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,46 +9,65 @@ class login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 64,
-              ),
-              TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: decorationTextfield.copyWith(
-                      hintText: "Enter Your Email : ")),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: decorationTextfield.copyWith(
-                    hintText: "Enter Your Password :",
-                  )),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(BTNgreen),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8))),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
                 ),
-                child: Text(
-                  "sign in",
-                  style: TextStyle(fontSize: 19),
+                TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: decorationTextfield.copyWith(
+                        hintText: "Enter Your Email : ")),
+                const SizedBox(
+                  height: 30,
                 ),
-              ),
-            ],
+                TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password :",
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                  child: Text(
+                    "sign in",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Do not have an account?",
+                        style: TextStyle(fontSize: 18)),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text('sign up',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18))),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
