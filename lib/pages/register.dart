dacd2beb80b1d7c06b9687b2e91f8ffc3f92b648
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 21:49:44 +0200

    import for the register.dart libraries

---
+++ lib/pages/register.dart
@@ -0,0 +1,4 @@
+import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/shared/colors.dart';
+import 'package:flower_app/shared/contants.dart';
+import 'package:flutter/material.dart';
