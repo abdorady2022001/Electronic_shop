a0f30a2e5844a86fd796ed6eecdc10e3013743bc
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 22:48:09 +0200

    Create a complete home page and AppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,8 +6,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("dasda"),
-    ));
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 76, 141, 95),
+        title: Text("Home"),
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$ 128"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
