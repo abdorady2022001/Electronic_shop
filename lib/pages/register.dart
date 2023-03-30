15818d06324ea6a681d069099d79b32099de8487
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 22:15:24 +0200

    How to make a screen scroll

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -15,67 +15,69 @@ class Register extends StatelessWidget {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(33.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 64,
-                ),
-                TextField(
-                    keyboardType: TextInputType.text,
-                    obscureText: false,
-                    decoration: decorationTextfield.copyWith(
-                      hintText: "Enter Your username : ",
-                    )),
-                const SizedBox(
-                  height: 30,
-                ),
-                TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    obscureText: false,
-                    decoration: decorationTextfield.copyWith(
-                        hintText: "Enter Your Email : ")),
-                const SizedBox(
-                  height: 30,
-                ),
-                TextField(
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: decorationTextfield.copyWith(
-                      hintText: "Enter Your Password :",
-                    )),
-                const SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(BTNgreen),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8))),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 64,
                   ),
-                  child: Text(
-                    "sign in",
-                    style: TextStyle(fontSize: 19),
+                  TextField(
+                      keyboardType: TextInputType.text,
+                      obscureText: false,
+                      decoration: decorationTextfield.copyWith(
+                        hintText: "Enter Your username : ",
+                      )),
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Do not have an account?",
-                        style: TextStyle(fontSize: 18)),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text('sign up',
-                            style:
-                                TextStyle(color: Colors.black, fontSize: 18))),
-                  ],
-                ),
-              ],
+                  TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: false,
+                      decoration: decorationTextfield.copyWith(
+                          hintText: "Enter Your Email : ")),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: decorationTextfield.copyWith(
+                        hintText: "Enter Your Password :",
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(BTNgreen),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                    ),
+                    child: Text(
+                      "sign in",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Do not have an account?",
+                          style: TextStyle(fontSize: 18)),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text('sign up',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 18))),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
