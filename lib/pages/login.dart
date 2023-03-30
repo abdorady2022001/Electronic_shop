159bb233f68ebce2337448e7f186726756325da4
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-30 12:01:28 +0200

    The secret number field

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,8 +7,33 @@ class login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [Text("data")],
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
+                  ))
+            ],
+          ),
         ),
       ),
     );
